101682ded93ce9ffcd0dd288e823e8b0f31f2ea1
Juanequex <ramon.g.uz37@gmail.com>
2024-03-25 14:10:46 -0600

    [seeds] custom seeds to test the app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,28 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# seeds
+
+[
+  {name: "Sumus"},
+  {name: "Wanabana"}
+].each do |company|
+  Company.where(name: company[:name]).first_or_create!
+  puts "Creating company: #{company[:name]}"
+end
+
+[
+  { company: Company.find_by(name: 'Sumus'), name: 'First quote' },
+  { company: Company.find_by(name: 'Sumus'), name: 'Second quote' },
+  { company: Company.find_by(name: 'Wanabana'), name: 'Third quote' }
+].each do |quote_attributes|
+  Quote.where(quote_attributes).first_or_create!
+  puts "Creating Quote: #{quote_attributes[:name]}"
+end
+
+[
+  { id: 1, email: 'accountant@sumus.com', password: 'password', created_at: Time.now, updated_at: Time.now, company_id: Company.find_by(name: 'Sumus').id },
+  { id: 2, email: 'manager@sumus.com', password: 'password', created_at: Time.now, updated_at: Time.now, company_id: Company.find_by(name: 'Sumus').id },
+  { id: 3, email: 'eavesdropper@wanabana.com', password: 'password', created_at: Time.now, updated_at: Time.now, company_id: Company.find_by(name: 'Wanabana').id }
+].each do |user_attributes|
+  user_name = user_attributes[:email].split("@").first.capitalize
+  puts "Creating User: #{user_name}"
+  User.where(email: user_attributes[:email]).first_or_create!(user_attributes.except(:email))
+end
