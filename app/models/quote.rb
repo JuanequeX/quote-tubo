880fa988a5dc4be412a863670a65a3b84d38eb37
juanequex <ramon.g.uz37@gmail.com>
2024-06-04 12:46:45 -0600

    [CRUD] Line Item dates without turbo frames and turbo streams

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
   belongs_to :company
+  has_many :line_item_dates, dependent: :destroy
 
   scope :ordered, -> { order(id: :desc) }
 
@@ -9,5 +10,4 @@ class Quote < ApplicationRecord
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
   # Those three callbacks are equivalent to the following single line 👇🏻
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
-
 end
